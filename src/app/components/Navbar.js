3e368794b01be3d74892ea72fe2441328aa02030
tomas-carrico-a22104735 <tomascarrico03@hotmail.com>
2023-06-17 12:27:25 +0100

    neptunestrategyNextjs

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,25 @@
+import styles from "../styles/navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <a href="">
+          <li>Home</li>
+        </a>
+        <a href="">
+          <li>About us</li>
+        </a>
+        <a href="">
+          <li>Services</li>
+        </a>
+        <a href="">
+          <li>Team</li>
+        </a>
+        <a href="">
+          <li>Contacts</li>
+        </a>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/app/pages/First.js
@@ -0,0 +1,9 @@
+import styles from "../styles/page.module.css";
+
+export default function Navbar() {
+  return (
+    <section className={styles.backImg}>
+        
+    </section>
+  );
+}
