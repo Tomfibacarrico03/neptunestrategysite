c77326cf05c6635853d208af87ee6b2a54358d45
tomas-carrico-a22104735 <tomascarrico03@hotmail.com>
2023-06-17 16:00:38 +0100

    .

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,14 @@ import styles from "./styles/page.module.css";
 import RootLayout from '../app/layout';
 import Navbar from '../app/components/Navbar'
 import First from '../app/pages/First';
+import Second from '../app/pages/Second';
+
 export default function Home() {
   return (
     <RootLayout>
       <Navbar/>
       <First/>
+      <Second/>
     </RootLayout>
   );
 }
--- src/app/pages/Second.js
+++ src/app/pages/Second.js
@@ -3,8 +3,8 @@ import Image from "next/image";
 import backgroundImage from "../images/imagemsite.png";
 export default function Navbar() {
   return (
-    <div className={styles.backImg}>
-      <Image src={backgroundImage} alt="My Image" />
+    <div className={styles.window} style={{zIndex:2}}>
+      teste
     </div>
   );
 }
